723b61d8494c9452161a9343c6c1aac977726537
giomadeira <giomadeirag@gmail.com>
2021-02-09 10:53:48 -0300

    aula 04.02

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+// sempre precisa de uma tag pai para encapsular as demais, neste caso é a <div>
+const User = () => {
+  return (
+      <div>
+        <p>Isa</p>
+        <p>Gosta de sorvete</p>
+    </div>
+  );
+}
+
+export default User;
